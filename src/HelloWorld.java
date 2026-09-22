17e7c278ea53787422a92715d064379aa1e5bd68
Raamiz Hussain Shikoh <raamizhs@gmail.com>
2026-09-22 23:33:58 +0530

    Fix HelloWorld.java syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,1 +1,5 @@
-public class HelloWorld {`n    public static void main(String[] args) {`n        System.out.println("Hello from Jenkins CI Pipeline!");`n    }`n}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI Pipeline!");
+    }
+}
